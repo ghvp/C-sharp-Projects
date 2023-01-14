5097ffee9b381b5366cccc30ad66da5e7c152450
ghvp <viviennepadilla14@gmail.com>
2023-01-14 00:58:08 -0800

    Income Comparison assignment

---
+++ ConsoleAppAssignment/ConsoleAppAssignment/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleAppAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            //START of multiplying a users input by 50!
+            Console.WriteLine("Please input a number below to multiply by 50");
+            string userNum1 = Console.ReadLine();
+            int num50 = Convert.ToInt32(userNum1);
+            int multBy50 = num50 * 50;
+            Console.WriteLine(num50.ToString() + " multiplied by 50 = " + userNum1.ToString());
+            Console.ReadLine();
+            //END 
+
+            //START of adding 25 to the user's input
+            Console.WriteLine("Please enter in a number below");
+            string userNum2
+        }
+    }
+}
---
+++ IncomeComparison/IncomeComparison/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace IncomeComparison
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Prints text to the console
+            Console.WriteLine("Anonymous Income Comparison Program");
+            Console.ReadLine();
+
+            //START PERSON 1 INFO
+            //Prints text to the console
+            Console.WriteLine("Person 1");
+            //Prompts the user to enter in their hourly rate
+            Console.WriteLine("Please enter your hourly rate");
+            //Allows the user to enter in their hourly rate
+            string Person1Hourly = Console.ReadLine();
+            //The user will enter in their Hours worked weekly
+            Console.WriteLine("Please enter your hours worked per week");
+            string Person1Weekly = Console.ReadLine();
+            //END OF PERSON 1 INFO
+
+
+            //START OF PERSON 2 INFO
+            //this chunk of code will follow the same format as 
+            //the code above and will have the same functionality
+            Console.WriteLine("Person 2");
+            Console.WriteLine("Please enter your hourly rate");
+            string Person2Hourly = Console.ReadLine();
+            Console.WriteLine("Please enter your hours worked per week");
+            string Person2Weekly = Console.ReadLine();
+            //END OF PERSON 2 INFO
+
+
+            //START OF DISPLAYING PERSON 1'S SALARY TO THE CONSOLE
+            //Prints text to the console
+            Console.WriteLine("Annual Salary of Person 1: ");
+            //Converting the variables for Person 1 into from strings
+            //to integers to allow for them to be multiplied
+            int P1Hourly = Convert.ToInt32(Person1Hourly);
+            int P1Weekly = Convert.ToInt32(Person1Weekly);
+            //Multiplies the converted variables above
+            int result1 = P1Weekly * P1Hourly;
+            //Prints Person 1's salary to the console as a string
+            Console.WriteLine(result1.ToString());
+            //END OF DISPLAYING PERSON 1'S SALARY
+
+
+            //START OF DISPLAYING PERSON 2'S SALARY TO THE CONSOLE
+            //Functions exactly the same as the block of code above.
+            Console.WriteLine("Annual Salary of Person 2: ");
+            int P2Hourly = Convert.ToInt32(Person2Hourly);
+            int P2Weekly = Convert.ToInt32(Person2Weekly);
+            int result2 = P2Weekly * P2Hourly;
+            Console.WriteLine(result2.ToString());
+
+
+            //START OF COMPARING THE SALARIES 
+            Console.WriteLine("Does Person 1 make more money than Person 2?");
+            bool compareSalary = result1 > result2;
+            Console.WriteLine(compareSalary.ToString());
+
+
+
+
+        }
+    }
+}
