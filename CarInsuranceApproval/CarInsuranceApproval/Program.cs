69838490b1de2f542b5008c614461d07e148f1ad
ghvp <viviennepadilla14@gmail.com>
2023-01-17 03:01:05 -0800

    Car insurance assignment

--- CarInsuranceApproval/CarInsuranceApproval/Program.cs
+++ CarInsuranceApproval/CarInsuranceApproval/Program.cs
@@ -16,14 +16,16 @@ namespace CarInsuranceApproval
             //Printing text to the console
             Console.WriteLine("Have you ever had a DUI? Please enter \'true\' or \'false'");
             //Allows user input
-            string DUI = Console.ReadLine();
+            bool DUI = false;
+            string HasDUI = Console.ReadLine();
+            //I'm not too sure how to implement this part.. would i convert the user input into a bool?
             //Applicant cannot have any DUIs 
 
             //Printing text to the console
             Console.WriteLine("How many speeding tickets do you have? (Please use numerical values)");
             //Allows user input
             string Tickets = Console.ReadLine();
-            //Applicant cannot have any speeding tickets 
+            //Applicant cannot have more than 3 speeding tickets 
             //END OF APPLICANT INFORMATION
 
 
@@ -34,6 +36,17 @@ namespace CarInsuranceApproval
             bool AgeOk = ApplicantAge >= 15;
 
             //converting DUI var to be accepted into a bool
+            //unsure how to implement.. see comment on line 21
+
+            //checking if applicant has more than 3 speeding tickets
+            int TotalTickets = Convert.ToInt32(Tickets);
+            bool TicketsOk = TotalTickets < 3;
+
+            //Checks if applicant qualifies 
+            bool Qualified = AgeOk && TicketsOk;
+            string ShowQualified = Convert.ToString(Qualified);
+            Console.WriteLine("Qualified?");
+            Console.WriteLine(Qualified);
 
             
 
