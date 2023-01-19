b9581655a0c983f42d87dee9ad0e1115dba4858f
ghvp <viviennepadilla14@gmail.com>
2023-01-19 03:14:36 -0800

    price quote assignment

---
+++ Branching/Branching/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace Branching
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your favorite number?");
+            int favNum = Convert.ToInt32(Console.ReadLine());
+
+            string result = favNum == 12 ? "You have an awesome favorite number" : "You do not have an awesome favorite number";
+
+            Console.WriteLine(result);
+
+            //int roomTemperature = 70;
+
+            //Console.WriteLine("Hi what is our name?");
+            //string name = Console.ReadLine();
+
+            //Console.WriteLine("Hi " + name + " what is the temperature where you are?");
+            //int currentTemperature = Convert.ToInt32(Console.ReadLine());
+
+            //if (currentTemperature == roomTemperature)
+            //{
+            //    Console.WriteLine("It is exactly room temperature");
+            //}
+            //else if (currentTemperature > roomTemperature)
+            //{
+            //    Console.WriteLine("It is warmer than room temperature");
+            //}
+            //else if (roomTemperature > currentTemperature)
+            //{
+            //    Console.WriteLine("it is colder than room temperature");
+            //}
+            //else 
+            //{
+            //    Console.WriteLine("uhhhhh.. something went wrong");
+            //} 
+
+            //int currentTemperature = 80;
+            //int roomTemperature = 70;
+
+            //string comparisonResult = currentTemperature == roomTemperature ? "it is room temp." : "it is not room temp";
+            //Console.WriteLine(comparisonResult);
+
+            //if (currentTemperature == roomTemperature)
+            //{
+            //    Console.WriteLine("It is exactly room temperature");
+            //}
+            //else if (currentTemperature > roomTemperature)
+            //{
+            //    Console.WriteLine("it is warmer than room temperature");
+            //}
+            //else if (roomTemperature > currentTemperature) 
+            //{
+            //    Console.WriteLine();
+            //}
+            //else
+            //{
+            //    Console.WriteLine("It is not exactly room temperature");
+            //}
+            Console.ReadLine();
+        }
+    }
+}
---
+++ PriceQuoteApp/PriceQuoteApp/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace PriceQuoteApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Greeting message
+            Console.WriteLine("Welcome to Package Express. Please follow the instructions below");
+
+            //Prompting user for package weight 
+            Console.WriteLine("Please enter in your package weight");
+            int acceptedWeight = 50;
+            int weight = Convert.ToInt32(Console.ReadLine());
+
+            //Creating an if statement that will display text if the parcel is too heavy to ship
+            if (weight > acceptedWeight) 
+            {
+                Console.WriteLine("Package too heavy to be shipped via Package Express. Have a nice day");
+                Environment.Exit(0);
+            }
+
+            //Prompting user for package width,height and length
+            Console.WriteLine("What is the width of your package");
+            int packageWidth = Convert.ToInt32(Console.ReadLine());
+            //Package height
+            Console.WriteLine("What is the height of your package");
+            int packageHeight = Convert.ToInt32(Console.ReadLine());
+            //Package length
+            Console.WriteLine("What is the length of your package");
+            int packageLength = Convert.ToInt32(Console.ReadLine());
+
+            //Checking to see if the package dimensions total greater than 50
+            //and if so, will display a message letting the user know 
+            //their package is too large to be shipped
+
+            //Variable to store the package dimensions 
+            int totalDimensions = packageHeight + packageLength + packageWidth;
+            //Variable that holds the max for dimensions 
+            int maxDimensions = 50;
+            //if statement that will display a message to the console if 
+            //package dimensions go over the max limit
+            if (totalDimensions > maxDimensions) 
+            {
+                Console.WriteLine("Package too big to be shipped via Package Express");
+                Environment.Exit(0);
+            }
+
+            //Multiplying the package dimensions 
+            int packageDimensions = packageWidth * packageHeight * packageLength;
+            //Stores the multiplied dimensions into a var dimensionAndWeight
+            // packageDimensions is then multiplied by weight
+            int dimensionAndWeight = packageDimensions * weight;
+            //price is calculated by dividing the dimensionAndWeight variable by 100
+            decimal Calculatedprice = dimensionAndWeight / 100m;
+            //displaying the quote to the user in dollar amount
+            Console.WriteLine("Your estimated total for shipping this package is: " + Calculatedprice);
+
+        }
+    }
+}
