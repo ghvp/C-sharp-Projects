3ce0a9ca26c72938fef182e798546a34be8dc02f
ghvp <viviennepadilla14@gmail.com>
2023-01-27 18:23:57 -0800

    Calling methods submission

---
+++ CallingMethodsSubmission/CallingMethodsSubmission/MyClass.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CallingMethodsSubmission
+{
+    class MyClass
+    {
+        //Creating my first method and allowing it to take the parameter var
+        public int Method1(int var) 
+        {
+            //creating a variable to display the total 
+            //of the user inputted number plus 20
+            //this method adds 5 to the user input
+            int firstTotal = var + 5;
+            //returning the total to the console
+            return firstTotal;
+        }
+        public int Method2(int var) 
+        {
+            //same function and format as the code block above
+            //this method subtracts 5 from the user input
+            int secondTotal = var - 5;
+            return secondTotal;
+        }
+        public int Method3(int var) 
+        {
+            //same function and format as the code block above
+            //this method multiplies the user input by 5
+            int thirdTotal = var * 5;
+            return thirdTotal;
+        }
+    }
+}
---
+++ CallingMethodsSubmission/CallingMethodsSubmission/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace CallingMethodsSubmission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("calling methods assignment");
+
+            //prompting the user to enter in a number to use
+            //for our methods
+            Console.WriteLine("Please enter in a number to use for the following equations. Addition, subtraction, and multiplication");
+            //Creating a variable to store the user's number
+            int userInput = Convert.ToInt32(Console.ReadLine());
+
+            
+            MyClass method = new MyClass();
+
+            //Calling the methods one at a time and displaying the answers to the console
+            int firstTotal = method.Method1(userInput);
+            Console.WriteLine(userInput + " plus " + 5 + " equals " + firstTotal);
+
+            int secondTotal = method.Method2(userInput);
+            Console.WriteLine(userInput + " minus " + 5 + " equals " + secondTotal);
+
+            int thirdTotal = method.Method3(userInput);
+            Console.WriteLine(userInput + " times " + 5 + " equals " + thirdTotal);
+
+            Console.ReadLine();
+        }
+        
+    }
+}
