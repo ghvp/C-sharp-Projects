d71b36e29febfdb9205c9ed4824b256c16323bfb
ghvp <viviennepadilla14@gmail.com>
2023-02-02 02:20:52 -0800

    Method Class Submission

---
+++ Method Class Submission/Method Class Submission/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Class_Submission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //asks user to enter in their first number
+            Console.WriteLine("Please enter in your first number");
+            double Usernum1 = Convert.ToDouble(Console.ReadLine());
+            //asks user to input their second number
+            Console.WriteLine("Now enter in your second number");
+            double Usernum2 = Convert.ToDouble(Console.ReadLine());
+
+            //methodOne is called from voidMethod 
+            //the first num is passed through an equation while the second num will
+            //display on its own
+            voidMethod method = new voidMethod();
+            method.methodOne(Usernum1, Usernum2);
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Method Class Submission/Method Class Submission/voidMethod.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Method_Class_Submission
+{
+    public class voidMethod
+    {
+        public void methodOne(double var1, double var2) 
+        {
+            double result = var1 * 3;
+            Console.WriteLine("{0}, times 3 equals " + result + ".", var1);
+            Console.WriteLine("{0} will not be included in any equation", var2);
+            return;
+        }
+    }
+}
