7437382f043991bd67ad0104189a6ae3557df5d8
ghvp <viviennepadilla14@gmail.com>
2023-02-24 05:32:47 -0800

    Final Challenge

---
+++ Final Challenge/Final Challenge/DBContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Final_Submission
+{
+    class SchoolContext : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+    }
+}
---
+++ Final Challenge/Final Challenge/Program.cs
@@ -0,0 +1,20 @@
+﻿using Final_Submission;
+using System;
+using System.Data.Entity;
+
+namespace Final_Challenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var ctx = new SchoolContext())
+            {
+                var stud = new Student() { StudentName = "Bill" };
+
+                ctx.Students.Add(stud);
+                ctx.SaveChanges();
+            }
+        }
+    }
+}
