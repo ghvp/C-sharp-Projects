a769e1e66dcde05e2ebcdd0c20233d4c81165256
ghvp <viviennepadilla14@gmail.com>
2023-02-23 22:30:11 -0800

    CarInsurance Initial commit
    Untouched project code just in case

--- NewsLetterAppMVC/NewsLetterAppMVC/Controllers/HomeController.cs
+++ NewsLetterAppMVC/NewsLetterAppMVC/Controllers/HomeController.cs
@@ -29,64 +29,18 @@ namespace NewsLetterAppMVC.Controllers
             }
             else 
             {
-
-                string queryString = @"INSERT INTO SignUps(FirstName, LastName, EmailAddress) VALUES 
-                                        (@FirstName, @LastName, @EmailAddress)";
-
-                using (SqlConnection connection = new SqlConnection(connectionString)) 
+                using (NewsLetterEntities db = new NewsLetterEntities()) 
                 {
-                    SqlCommand command = new SqlCommand(queryString, connection);
-                    command.Parameters.Add("@FirstName", SqlDbType.VarChar);
-                    command.Parameters.Add("@LastName", SqlDbType.VarChar);
-                    command.Parameters.Add("@EmailAddress", SqlDbType.VarChar);
+                    var signup = new SignUp();
+                    signup.FirstName = firstName;
+                    signup.LastName = lastName;
+                    signup.EmailAddress = emailAddress;
 
-                    command.Parameters["@FirstName"].Value = firstName;
-                    command.Parameters["@LastName"].Value = lastName;
-                    command.Parameters["@EmailAddress"].Value = emailAddress;
-
-                    connection.Open();
-                    command.ExecuteNonQuery();
-                    connection.Close();
+                    db.SignUps.Add(signup);
+                    db.SaveChanges();
                 }
                 return View("Success");
             }
-        }
-
-        public ActionResult Admin() 
-        {
-            string queryString = @"SELECT Id, FirstName, LastName, EmailAddress, SocialSecurityNumber from SignUps";
-            List<NewsLetterSignUp> signups = new List<NewsLetterSignUp>();
-
-            using (SqlConnection connection = new SqlConnection(connectionString)) 
-            {
-                SqlCommand command = new SqlCommand(queryString, connection);
-
-                connection.Open();
-                SqlDataReader reader = command.ExecuteReader();
-
-                while (reader.Read()) 
-                {
-                    var signup = new NewsLetterSignUp();
-                    signup.Id = Convert.ToInt32(reader["Id"]);
-                    signup.FirstName = reader["FirstName"].ToString();
-                    signup.LastName = reader["LastName"].ToString();
-                    signup.EmailAddress = reader["EmailAddress"].ToString();
-                    signup.SocialSecurityNumber = reader["SocialSecurityNumber"].ToString();
-
-                    signups.Add(signup);
-                }
-            }
-            var signupVMs = new List<SignupVM>();
-            foreach (var signup in signups) 
-            {
-                var signupVM = new SignupVM();
-                signupVM.FirstName = signup.FirstName;
-                signupVM.LastName = signup.LastName;
-                signupVM.EmailAddress = signup.EmailAddress;
-                signupVMs.Add(signupVM);
-            }
-
-            return View(signupVMs);
         }
     }
 }
